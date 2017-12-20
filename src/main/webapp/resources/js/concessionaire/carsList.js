004a14e86fd6711b2a779607fb881bb212418265
root <pablotr87@gmail.com>
2017-12-20 19:06:34 +0100

    Fixes #1

--- src/main/webapp/resources/js/concessionaire/carsList.js
+++ src/main/webapp/resources/js/concessionaire/carsList.js
@@ -1,14 +1,19 @@
 $(function () {
-    var table = $("#carsListTb");
-    registerTableEvents(table);
+    var $table = $("#carsListTb");
+    registerTableEvents($table);
+    $(".btn-spreadsheet").prop("disabled", true);
 });
 
 /**
  * Register table events.
- * @param table Table element.
+ * @param $table Table element.
  */
-function registerTableEvents(table) {
-    table.on("dbl-click-row.bs.table", function (e, row) {
+function registerTableEvents($table) {
+    $table.on("dbl-click-row.bs.table", function (e, row) {
         window.location.href = "detail/" + row.id;
+    });
+
+    $table.on('load-success.bs.table', function () {
+        $(".btn-spreadsheet").prop("disabled", false);
     });
 }
